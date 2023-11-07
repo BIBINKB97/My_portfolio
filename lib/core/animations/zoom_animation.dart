3fbe36e166a14a16f65885831a1a721772fded60
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-07 20:56:01 +0530

    made some changes

--- lib/core/animations/zoom_animation.dart
+++ lib/core/animations/zoom_animation.dart
@@ -62,38 +62,35 @@ class _ZoomAnimationsState extends State<ZoomAnimations>
     return SizedBox(
       width: size.width / 4,
       height: size.width / 4,
-      child: AlignTransition(
-        alignment: _alignAnimation,
-        child: CustomOutline(
-          strokeWidth: 5,
-          radius: size.width * 0.2,
-          padding: const EdgeInsets.all(5),
-          width: size.width * sizeAnimation.value,
-          height: size.width * sizeAnimation.value,
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                theme.secondaryColor,
-                theme.secondaryColor.withOpacity(0),
-                theme.primaryColor.withOpacity(0.1),
-                theme.primaryColor
-              ],
-              stops: const [
-                0.2,
-                0.4,
-                0.6,
-                1
-              ]),
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.black.withOpacity(0.8),
-              image: const DecorationImage(
-                fit: BoxFit.cover,
-                alignment: Alignment.bottomLeft,
-                image: AssetImage('assets/images/IMG_0107.png'),
-              ),
+      child: CustomOutline(
+        strokeWidth: 5,
+        radius: size.width * 0.2,
+        padding: const EdgeInsets.all(5),
+        width: size.width * sizeAnimation.value,
+        height: size.width * sizeAnimation.value,
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              theme.secondaryColor,
+              theme.secondaryColor.withOpacity(0),
+              theme.primaryColor.withOpacity(0.1),
+              theme.primaryColor
+            ],
+            stops: const [
+              0.2,
+              0.4,
+              0.6,
+              1
+            ]),
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Colors.black.withOpacity(0.8),
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              alignment: Alignment.bottomLeft,
+              image: AssetImage('assets/images/me.jpeg'),
             ),
           ),
         ),
