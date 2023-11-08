5f669eb915c247b621dad08be53b86ec9738b6ac
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-08 15:21:16 +0530

    made some changes

--- lib/changes/links.dart
+++ lib/changes/links.dart
@@ -17,11 +17,11 @@ const String buymeacoffee = 'https://www.buymeacoffee.com/sudeshnb';
 
 const String instagram = 'https://instagram.com/sudesh_nish';
 
-const String profileImage =
-    "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+// const String profileImage =
+//     "";
 
 const String fiverr = 'https://www.fiverr.com/sudeshnb';
 
 const String upwork = 'https://www.upwork.com/freelancers/~015c46144016569e31';
 
-const String whatsapp = 'https://api.whatsapp.com/send?phone=94741325674';
+const String whatsapp = 'https://api.whatsapp.com/send?phone=8943830640';
