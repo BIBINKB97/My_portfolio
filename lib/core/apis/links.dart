263007ff15b9137c11f35e4a13aa0dae901cd411
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-09 21:29:11 +0530

    made some changes

--- lib/core/apis/links.dart
+++ lib/core/apis/links.dart
@@ -1,12 +1,3 @@
-// const List<String> socialIconURL = [
-//   "https://img.icons8.com/fluency/48/000000/facebook-new.png",
-//   // "https://img.icons8.com/color/48/000000/instagram-new--v1.png",
-//   "https://img.icons8.com/color/48/000000/youtube-play.png",
-//   "https://img.icons8.com/fluency/48/000000/twitter.png",
-//   "https://img.icons8.com/color/48/000000/linkedin-circled--v1.png",
-//   "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-buy-me-a-coffee-help-creators-receive-support-from-their-audience-logo-shadow-tal-revivo.png",
-//   "https://img.icons8.com/fluency/48/000000/github.png",
-// ];
 
 class IconUrls {
   static String upworkUrl =
