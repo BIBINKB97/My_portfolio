0aa59615f42f3a3b598072a207f65ad1d1ef03cb
bibin k b <bibinkochumalayil007@gmail.com>
2023-11-09 22:24:32 +0530

    changed project utils

--- lib/app/utils/project_utils.dart
+++ lib/app/utils/project_utils.dart
@@ -19,31 +19,31 @@ List<ProjectUtils> projectUtils = [
     icons: 'assets/images/flutter.png',
     titles: 'Awesome Money management application',
     description:
-        'This is a Fully functional Money management application App by using flutter, source code is also available, check below.',
+        'A Fully functional Money management application App by using flutter, source code is also available, check below.',
     links: 'https://github.com/BIBINKB97/BillsBook-with-Provider',
   ),
   ProjectUtils(
     banners: 'assets/images/03.png',
     icons: 'assets/images/flutter.png',
-    titles: 'Awesome Todo application',
+    titles: 'Todo application',
     description:
-        'This a Todo application using Flutter, source code is also available, check below.',
-    links: 'https://github.com/sudeshnb/',
+        'A Todo application using Flutter, source code is also available, check below.',
+    links: 'https://github.com/BIBINKB97/Todo_app_with_Hive',
   ),
   ProjectUtils(
     banners: 'assets/images/03.png',
     icons: 'assets/images/flutter.png',
     titles: 'Exercise App',
     description:
-        'This is a just Fully function Exercise App by using flutter, source code is also available, check below.',
-    links: 'https://github.com/sudeshnb/elderly_exercise_app.git',
+        '',
+    links: '',
   ),
   ProjectUtils(
     banners: 'assets/images/04.png',
     icons: 'assets/images/flutter.png',
-    titles: 'Awesome Plant App UI',
+    titles: 'Chat Application',
     description:
-        'This is a just Plant App UI by using flutter, source code is also available, check below.',
-    links: 'https://github.com/sudeshnb/flutter_plant_app.git',
+        'This is a Chat Application  using flutter, source code is also available, check below.',
+    links: 'https://github.com/BIBINKB97/Talkaro_chatApp',
   ),
 ];
